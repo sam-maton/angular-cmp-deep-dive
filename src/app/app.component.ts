69c0df92118a4cec2514933d9c66c611acadefc4
sam-maton <sam.maton91@gmail.com>
2025-05-02 20:39:57 +0100

    setup dashboard item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,6 +13,7 @@ import { TicketsComponent } from './dashboard/tickets/tickets.component';
     ServerStatusComponent,
     TrafficComponent,
     TicketsComponent,
+    DashboardItemComponent,
   ],
 })
 export class AppComponent {}
